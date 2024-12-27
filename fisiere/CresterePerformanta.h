efed24c84569234ff8b871d4ce62c864237fe652
danaioa <danaioana104@gmail.com>
2024-12-27 13:22:08 +0200

    check

--- fisiere/CresterePerformanta.h
+++ fisiere/CresterePerformanta.h
@@ -1,17 +1,14 @@
-#ifndef CRESTERE_PERFORMANTA_H
-#define CRESTERE_PERFORMANTA_H
+#ifndef CRESTEREPERFORMANTA_H
+#define CRESTEREPERFORMANTA_H
 
 #include "Obiectiv.h"
 #include <string>
 
 class CresterePerformanta : public Obiectiv {
 public:
-    CresterePerformanta() = default;
-    ~CresterePerformanta() override = default;
-
     [[nodiscard]] std::string getObiectivString() const override;
 
-    [[nodiscard]] static int calculeazaCalorii(int greutate,  int varsta, int gen) ;
+    [[nodiscard]] int calculeazaCalorii(int greutate, int inaltime, int varsta, int gen) const override;
 };
 
-#endif // CRESTERE_PERFORMANTA_H
+#endif // CRESTEREPERFORMANTA_H
